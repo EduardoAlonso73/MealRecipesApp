4df0e8e4d638688a95481c13ced4ed0168483962
eduardo <eduardo.alonso.73h@gmail.com>
2025-04-27 17:10:37 -0600

    Improve splash screen

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -9,22 +9,46 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Color(0xFFFE9635),
-      child: TweenAnimationBuilder<double>(
-        tween: Tween<double>(begin: -0.6, end: 0.0),
-        duration: const Duration(milliseconds: 2000),
-        curve: Curves.easeInOut,
-        onEnd: () {
-          Navigator.pushReplacementNamed(context, AppRoute.home.route);
-        },
-        builder: (BuildContext context, double value, Widget? child) {
-          return Transform.translate(
-            offset: Offset(0, MediaQuery.of(context).size.height * value),
-            child: Image.asset(
-              'assets/ic_app.png',
-              fit: BoxFit.none,
+      width: double.infinity,
+      height: double.infinity,
+      child: Stack(
+        children: [
+          Center(
+            child: TweenAnimationBuilder<double>(
+              tween: Tween<double>(begin: -0.6, end: 0.0),
+              duration: const Duration(milliseconds: 2000),
+              curve: Curves.easeInOut,
+              onEnd: () {
+                Navigator.pushReplacementNamed(context, AppRoute.home.route);
+              },
+              builder: (BuildContext context, double value, Widget? child) {
+                return Transform.translate(
+                  offset: Offset(0, MediaQuery.of(context).size.height * value),
+                  child: Image.asset(
+                    'assets/ic_app.png',
+                    fit: BoxFit.none,
+                  ),
+                );
+              },
             ),
-          );
-        },
+          ),
+          // Indicador de progreso en bottomCenter
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 32.0),
+              // espacio desde abajo
+              child: SizedBox(
+                width: 40,
+                height: 40,
+                child: CircularProgressIndicator(
+                  color: Colors.white,
+                  strokeCap: StrokeCap.round,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
